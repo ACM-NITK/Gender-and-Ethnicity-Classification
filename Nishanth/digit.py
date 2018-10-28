081902c8432c83e0501ca4185283a3956223a7ba
NishanthSubramanian <nishanth80442@gmail.com>
2018-10-29 01:23:34 +0530

    Added digit recognition

---
+++ Nishanth/digit.py
@@ -0,0 +1,43 @@
+import numpy
+import tensorflow as tf
+from tensorflow import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Conv2D
+from keras.layers.convolutional import MaxPooling2D
+from keras.optimizers import Adam
+from keras.utils import np_utils
+
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+
+X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], 1).astype('float32')
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], 1).astype('float32')
+
+X_train/=255
+X_test/=255
+
+# one hot encode
+number_of_classes = 10
+y_train = np_utils.to_categorical(y_train, number_of_classes)
+y_test = np_utils.to_categorical(y_test, number_of_classes)
+
+# create model
+model = Sequential()
+model.add(Conv2D(32, (5, 5), input_shape=(X_train.shape[1], X_train.shape[2], 1), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(32, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.2))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(number_of_classes, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=10, batch_size=200)
+model.save('models/mnistCNN.h5')
+metrics = model.evaluate(X_test, y_test, verbose=0)
+print("Metrics(Test loss & Test Accuracy): ")
+print(metrics)
